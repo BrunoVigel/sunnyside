5ab4349f2a6890ce06f6fdc315817e4edfa5c6e0
BrunoVigel <brunorosav@gmail.com>
2021-07-13 10:27:10 -0300

    fourth commit

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ botao.addEventListener('click', function () {
     behavior: 'smooth'
   })
 })
+
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
